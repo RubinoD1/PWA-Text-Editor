68d5e036a19a83647c52e30210eea8b8e7018f6f
RubinoD1 <Rubinod1@gmail.com>
2022-09-10 21:45:23 -0400

    added CSS loaders and babel settings  to webpack.config.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -8,9 +8,11 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
-  });
+});
