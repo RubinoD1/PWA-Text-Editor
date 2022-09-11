6363b800924a2d548ed80725bc1cd7d4c7e1d1e9
RubinoD1 <Rubinod1@gmail.com>
2022-09-10 22:24:32 -0400

    added and configured workbox plugins for sw and manifest file in webpack.config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 
 
 module.exports = () => {
@@ -17,9 +17,35 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
-    ],
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: 'pwa-text-editor',
+          short_name: 'Jate',
+          description: 'A basic text editor',
+          background_color: '#7eb4e2',
+          theme_color: '#7eb4e2',
+          start_url: './',
+          publicPath: './',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            }
+          ],
+        }), 
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+        }), 
+      ],
     // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
