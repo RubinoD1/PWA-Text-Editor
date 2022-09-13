eeef926dc50695cfe61f76e9efe5509e05f5d0e7
RubinoD1 <Rubinod1@gmail.com>
2022-09-13 19:15:58 -0400

    fixed webpack errors and install now working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 
+
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,50 +20,55 @@ module.exports = () => {
     // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Webpack Plugin',
+        template: "./index.html",
+        title: "JATE",
       }),
-      new WebpackPwaManifest({
-          fingerprints: false,
-          inject: true,
-          name: 'pwa-text-editor',
-          short_name: 'Jate',
-          description: 'A basic text editor',
-          background_color: '#7eb4e2',
-          theme_color: '#7eb4e2',
-          start_url: './',
-          publicPath: './',
-          icons: [
-            {
-              src: path.resolve('src/images/logo.png'),
-              sizes: [96, 128, 192, 256, 384, 512],
-              destination: path.join('assets', 'icons'),
-            }
-          ],
-        }), 
         new InjectManifest({
-          swSrc: './src-sw.js',
-          swDest: 'src-sw.js',
-        }), 
-      ],
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "pwa-text-editor",
+        short_name: "JATE",
+        description: "A basic text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
     // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
-          test: /\.css$/i,
+          test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
-        },
+        }
+        
       ],
     },
   };
